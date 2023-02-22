c1d65f4d26f63911c322fa81b7ca746ed0f24ab1
Taylir <110134876+Taylir@users.noreply.github.com>
2023-02-22 18:03:37 -0500

    Added some user interface to let people know the usage of this blog

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,19 +16,59 @@ export default function Header() {
             />
           </Link>
           <div className={styleUtils.nav__menu}>
-            <h3>About</h3>
-            <h3>Contact</h3>
-            <h3>Follow</h3>
+            <h3
+              onClick={() =>
+                alert(
+                  "This is currently just a personal blog show casing the blog aspect. In the future I will make something like that though!"
+                )
+              }
+            >
+              About
+            </h3>
+            <h3
+              onClick={() =>
+                alert(
+                  "This is currently just a personal blog show casing the blog aspect. In the future I will make something like that though!"
+                )
+              }
+            >
+              Contact
+            </h3>
+            <h3
+              onClick={() =>
+                alert(
+                  "This is currently just a personal blog show casing the blog aspect. In the future I will make something like that though!"
+                )
+              }
+            >
+              Follow
+            </h3>
           </div>
         </div>
         <div className={styleUtils.lines__wrapper}>
-            <div className={styleUtils.line}></div>
-            <div className={styleUtils.line}></div>
-            <div className={styleUtils.line}></div>
+          <div className={styleUtils.line}></div>
+          <div className={styleUtils.line}></div>
+          <div className={styleUtils.line}></div>
         </div>
         <div className={styleUtils.nav__right}>
-          <h3>Log in</h3>
-          <h3>Get Started</h3>
+          <h3
+            onClick={() =>
+              alert(
+                "This is currently just a personal blog show casing the blog aspect. In the future I will make something like that though!"
+              )
+            }
+          >
+            Log in
+          </h3>
+          <h3
+            onClick={() =>
+              alert(
+                "This is currently just a personal blog show casing the blog aspect. In the future I will make something like that though!"
+              )
+            }
+          >
+            Get Started
+          </h3>
         </div>
       </header>
     </>
